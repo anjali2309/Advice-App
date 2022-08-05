a84165443afbcc734aa8645905660ac2650d2dbc
Anjali <anjali_12015035@nitkkr.ac.in>
2022-08-05 16:13:44 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+//import axios from 'axios';
+import { useEffect, useState } from 'react';
+
+const App = () => {
+  const [advice, setAdvice] = useState('');
+
+  const fetchAdvice = async() => {
+    const response = await fetch(`https://api.adviceslip.com/advice`);
+    
+    if(!response.ok) console.log('Error');
+    else{
+      const data = await response.json();
+      const slip = data.slip.advice;
+      setAdvice(slip);
+    }
+  }
+
+  return (
+    <div className="App">
+      <div className="card">
+        <h1 className="heading">{advice}</h1>
+        <button className='button' onClick={fetchAdvice}>
+          <span>GIVE ME ADVICE!</span>
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
